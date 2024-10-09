6a7686c939ece63b2c5b295be5c5f023180c727f
huy51535 <77768240+huy51535@users.noreply.github.com>
2024-10-09 18:30:36 +0700

    .

---
+++ Assets/Eternal Warriors/Scenes/main menu/continue.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class BlinkingText : MonoBehaviour
+{
+    public TextMeshProUGUI clickText;
+    private bool isBlinking = true;
+    public float blinkInterval = 0.5f;
+
+    private void Start()
+    {
+        StartCoroutine(BlinkText());
+    }
+
+    private IEnumerator BlinkText()
+    {
+        while (isBlinking)
+        {
+            clickText.text = "Click to Continue";
+            yield return new WaitForSeconds(blinkInterval);
+            clickText.text = "";
+            yield return new WaitForSeconds(blinkInterval);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            isBlinking = false;
+            
+            ChangeScene();
+        }
+    }
+
+    private void ChangeScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("NextScene"); 
+    }
+}
