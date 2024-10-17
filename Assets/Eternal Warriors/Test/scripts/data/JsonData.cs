5e80a93746e5a382e10af9c2f915f472a0b76ce2
nminh123 <caonhatminh245@gmail.com>
2024-10-17 08:48:45 +0700

    Core test

---
+++ Assets/Eternal Warriors/Test/scripts/data/JsonData.cs
@@ -0,0 +1,19 @@
+namespace Test.scripts.data.core
+{
+    public struct JsonData
+    {
+        int _score;
+
+        public int score
+        {
+            get
+            {
+                return score;
+            }
+            set
+            {
+                _score = value;
+            }
+        }
+    }
+}
---
+++ Assets/Eternal Warriors/Test/scripts/data/Target.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+namespace Test.scripts.data.core
+{
+    [RequireComponent(typeof(CircleCollider2D))]
+    public class Target : MonoBehaviour
+    {
+        private CircleCollider2D collision;
+
+        private void Start()
+        {
+            collision = GetComponent<CircleCollider2D>();
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
