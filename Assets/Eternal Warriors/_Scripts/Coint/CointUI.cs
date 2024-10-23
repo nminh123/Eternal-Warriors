3cb2048137da1803ba499065eea470872d369b64
Fjnnn2907 <phuongphipho2005@gmail.com>
2024-10-23 20:28:56 +0700

    leanTween Coint

--- Assets/Eternal Warriors/_Scripts/Coint/CointUI.cs
+++ Assets/Eternal Warriors/_Scripts/Coint/CointUI.cs
@@ -1,15 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
+﻿using TMPro;
 using UnityEngine;
 
 public class CointUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textCoint;
+    private int displayedCoint = 0;
+    private float increaseSpeed = 20f;
+
+    private void Start()
+    {
+        displayedCoint = CointManager.instance.coint;
+        textCoint.text = displayedCoint.ToString();
+    }
 
     private void Update()
     {
-        textCoint.text = CointManager.instance.coint.ToString();
+        if (displayedCoint < CointManager.instance.coint)
+        {
+
+            displayedCoint += Mathf.CeilToInt(increaseSpeed * Time.deltaTime);
+
+            if (displayedCoint > CointManager.instance.coint)
+                displayedCoint = CointManager.instance.coint;
+
+
+            textCoint.text = displayedCoint.ToString();
+        }
     }
-    
 }
