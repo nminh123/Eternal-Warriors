718ad111fc08b8addce9b938a9111749f43f8307
nminh123 <caonhatminh245@gmail.com>
2024-10-25 22:29:22 +0700

    add newtonsoft.json

--- Assets/Eternal Warriors/_Scripts/ChangeScene/ChangeScene.cs
+++ Assets/Eternal Warriors/_Scripts/ChangeScene/ChangeScene.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static CyclesProvider;
 using UnityEngine.SocialPlatforms.Impl;
 
 public class ChangeScene : MonoBehaviour
