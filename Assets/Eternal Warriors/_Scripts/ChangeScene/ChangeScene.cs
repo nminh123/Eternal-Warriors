870657d78f1c18de2cfafbd67c4ab297e0a180b9
Minh280123 <127473811+Minh280123@users.noreply.github.com>
2024-10-25 21:22:48 +0700

    sound

--- Assets/Eternal Warriors/_Scripts/ChangeScene/ChangeScene.cs
+++ Assets/Eternal Warriors/_Scripts/ChangeScene/ChangeScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static CyclesProvider;
+//using static CyclesProvider;
 using UnityEngine.SocialPlatforms.Impl;
 
 public class ChangeScene : MonoBehaviour
