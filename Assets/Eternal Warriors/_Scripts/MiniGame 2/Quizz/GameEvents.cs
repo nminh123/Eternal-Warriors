93d0c72d4091eb402b5b9ce9b43c8fc72a6ae98b
nminh123 <caonhatminh245@gmail.com>
2024-10-29 12:08:53 +0700

    Update Quizz game core

---
+++ Assets/Eternal Warriors/_Scripts/MiniGame 2/Quizz/GameEvents.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Eternal_Warriors._Scripts.MiniGame_2.Quizz
+{
+    [CreateAssetMenu(fileName = "GameEvents", menuName = "Quiz/ new GameEvents")]
+    public class GameEvents : ScriptableObject
+    {
+        
+    }
+}
---
+++ Assets/Eternal Warriors/_Scripts/MiniGame 2/Quizz/Question.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Eternal_Warriors._Scripts.MiniGame_2.Quizz
+{
+    public class Question : ScriptableObject
+    {
+        [SerializeField] private string info = string.Empty;
+        public string Info
+        {
+            get { return info; }
+        }
+
+        [SerializeField] private Answer[] answers = null;
+        public Answer[] Answers
+        {
+            get { return answers; }
+        }
+
+        [SerializeField] private bool userTimer = false;
+        public bool UseTimer
+        {
+            get { return userTimer; }
+        }
+
+        [SerializeField] private int timer = 0;
+        public int Timer
+        {
+            get { return timer; }
+        }
+
+        [SerializeField] private AnswerType answerType = AnswerType.Multi;
+        public AnswerType GetAnswerType
+        {
+            get { return answerType; }
+        }
+
+        [SerializeField] private int addScore = 10;
+        public int AddScore
+        {
+            get { return addScore; }
+        }
+
+        public List<int> GetCorrectAnswers()
+        {
+            List<int> CorrectAnswers = new List<int>();
+
+            for (int i = 0; i < answers.Length; i++)
+            {
+                if (answers[i].IsCorrect)
+                {
+                    CorrectAnswers.Add(i);
+                }
+            }
+
+            return CorrectAnswers;
+        }
+    }
+}
