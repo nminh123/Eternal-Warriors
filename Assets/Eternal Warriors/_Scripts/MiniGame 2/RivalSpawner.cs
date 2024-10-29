5f8611a8abc890f5a4694363700d90f22f402bb5
nminh123 <caonhatminh245@gmail.com>
2024-10-29 09:34:11 +0700

    Minigame 2 core

--- Assets/Eternal Warriors/_Scripts/MiniGame 2/RivalSpawner.cs
+++ Assets/Eternal Warriors/_Scripts/MiniGame 2/RivalSpawner.cs
@@ -6,7 +6,8 @@ namespace Eternal_Warriors._Scripts.MiniGame_2
     public class RivalSpawner : MonoBehaviour
     {
         [SerializeField] private GameObject prefab;
-        [SerializeField] private float maxX, minX, maxY, minY, timeBetweenSpawn, spawnTime;
+        [SerializeField] private float maxX, minX, maxY, minY, timeBetweenSpawn;
+        private float spawnTime;
 
         private void Update()
         {
@@ -22,7 +23,7 @@ namespace Eternal_Warriors._Scripts.MiniGame_2
             float x = Random.Range(minX, maxX);
             float y = Random.Range(minY, maxY);
 
-            Instantiate(prefab, new Vector3(x, y, 0), transform.rotation);
+            Instantiate(prefab, transform.position + new Vector3(x, y, 0), transform.rotation);
         }
     }
 }
