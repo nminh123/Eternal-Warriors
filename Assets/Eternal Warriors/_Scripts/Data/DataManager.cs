a64745d651ebb833f67e346f1a92ffe041c80e2f
nminh123 <caonhatminh245@gmail.com>
2024-11-01 03:12:31 +0700

    đổi JsonUtility thành sử dụng package Newtonsoft.Json, hoàn thành lưu dữ liệu.

--- Assets/Eternal Warriors/_Scripts/Data/DataManager.cs
+++ Assets/Eternal Warriors/_Scripts/Data/DataManager.cs
@@ -1,74 +1,49 @@
 using System.IO;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace Eternal_Warriors._Scripts.Data
 {
     public class DataManager : MonoBehaviour
     {
-        private string path;
-
         [SerializeField] private GameManager manager;
-        private DataJson data;
-
-        private void CreateFile()
-        {
-            if (File.Exists(path))
-                return;
-            using (FileStream fs = File.Create(path))
-            {
-                Debug.Log("FIle created succesfully!!");
-            }
-        }
-        
-        public void SaveGame()
-        {
-            if(!File.Exists(path))
-                Debug.LogWarning("File not found!");
-            if(data == null)
-                Debug.LogWarning("Data is null!!");
-            else
-            {
-                data.coin = manager.Coin;
-                string json = JsonUtility.ToJson(data, true);
-                Debug.Log(json);
-                File.WriteAllText(path, json);
-            }
-        }
-
-        public void LoadGame()
-        {
-            if (!File.Exists(path))
-            {
-                Debug.LogWarning("File not found!");
-                SaveGame();
-            }
-            if(data == null)
-                Debug.LogWarning("Data is null!!");
-            else
-            {
-                string json = File.ReadAllText(path);
-                data = JsonUtility.FromJson<DataJson>(json);
-
-                manager.Coin = data.coin;
-            }
-        }
-        
-        private void Awake()
-        {
-            data = new DataJson();
-            path = Application.persistentDataPath + "/data/dat.json";
-            Debug.Log(path);
-        }
+        [SerializeField] private SerializationData data;
+        private string path = Application.dataPath + "/savedata.json";
 
         private void Start()
         {
-            CreateFile();
-            LoadGame();
+            Load();
         }
-        
+
         private void OnApplicationQuit()
         {
-            SaveGame();
+            Save();
+        }
+
+        public void Save()
+        {
+            data.coin = manager.Coin;
+            data.Time = manager.DateTime;
+    
+            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
+            File.WriteAllText(path, json);
+            Debug.Log("Saved Data: " + json);
+        }
+
+        public void Load()
+        {
+            if (File.Exists(path))
+            {
+                string json = File.ReadAllText(path);
+                Debug.Log("Loaded Data: " + json);
+            
+                data = JsonConvert.DeserializeObject<SerializationData>(json);
+
+                manager.Coin = data.coin;
+                manager.DateTime = data.Time;
+            }
+            else
+                Debug.LogWarning("Save file not found. Initializing with default values.");
         }
     }
 }
