0afafabb7b8f0de67a7b45aa26ec47f16f6c26af
huy51535 <77768240+huy51535@users.noreply.github.com>
2024-11-05 02:30:19 +0700

    lf

---
+++ Assets/Eternal Warriors/Scenes/MiniGames/gia gao/dimanoichuyenvoithanggialkiadiconmemay/mousemotherfucker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShowGameObjectOnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject hoverObject; // GameObject sẽ hiển thị khi di chuột vào Button
+
+    void Start()
+    {
+        // Đảm bảo GameObject bị ẩn khi bắt đầu
+        if (hoverObject != null)
+        {
+            hoverObject.SetActive(false);
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Hiển thị GameObject khi di chuột vào Button
+        if (hoverObject != null)
+        {
+            hoverObject.SetActive(true);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // Ẩn GameObject khi rời chuột khỏi Button
+        if (hoverObject != null)
+        {
+            hoverObject.SetActive(false);
+        }
+    }
+}
