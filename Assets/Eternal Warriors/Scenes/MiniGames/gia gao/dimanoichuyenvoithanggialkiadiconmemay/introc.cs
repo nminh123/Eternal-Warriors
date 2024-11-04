f620b3fcd21fdbfceb052a6bff461f25534581ee
huy51535 <77768240+huy51535@users.noreply.github.com>
2024-11-05 01:13:14 +0700

    .

---
+++ Assets/Eternal Warriors/Scenes/MiniGames/gia gao/dimanoichuyenvoithanggialkiadiconmemay/introc.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class IntroController : MonoBehaviour
+{
+    private bool isIntroActive = true;
+    private int currentLine = 0;
+
+    public string[] introLines;
+    public TextMeshProUGUI introText; 
+    public string nextSceneName; 
+
+    void Start()
+    {
+        DisplayCurrentLine();
+    }
+
+    void Update()
+    {
+        if (isIntroActive)
+        {
+            if (Input.GetKeyDown(KeyCode.E) || Input.GetMouseButtonDown(0))
+            {
+                AdvanceIntro();
+            }
+        }
+    }
+
+    void DisplayCurrentLine()
+    {
+        if (introLines != null && currentLine < introLines.Length)
+        {
+            introText.text = introLines[currentLine];
+        }
+        else
+        {
+            EndIntro();
+        }
+    }
+
+    void AdvanceIntro()
+    {
+        currentLine++;
+
+        if (currentLine < introLines.Length)
+        {
+            DisplayCurrentLine();
+        }
+        else
+        {
+            EndIntro();
+        }
+    }
+
+    void EndIntro()
+    {
+        isIntroActive = false;
+
+        if (!string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+    }
+}
